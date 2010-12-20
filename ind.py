d78584845b2a43d9843b9d812427d229eecae762
Dan Eggleston <eggled@gmail.com>
2010-12-20 06:57:38 -0600

    Added threading to a gtk interface...

--- ind.py
+++ ind.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
-import gtk,time
+import gtk,time,threading
 
+def watchwpa():
+	print "Doing my sleeping"
+	time.sleep(5)
+	print "All done!"
+	return
+
+k = threading.Thread(target=watchwpa)
+k.start()
+
+gtk.gdk.threads_init()
 n = gtk.StatusIcon()
 n.set_from_file("/usr/share/pixmaps/gwibber.svg")
 n.set_tooltip_text("Egg-Wireless")

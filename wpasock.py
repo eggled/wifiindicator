1025874614049dc77734241ff816603875b7509a
Dan Eggleston <eggled@gmail.com>
2010-12-20 10:33:56 -0600

    Classifying

--- wpasock.py
+++ wpasock.py
@@ -2,32 +2,6 @@
 
 import socket,os,sys,re
 
-def bssidHandle(val):
-	wpamon.setbssid(val)
-	return
-def ssidHandle(val):
-	wpamon.setssid(val)
-	return
-def mgmtHandle(val):
-	wpamon.setkeymgmt(val)
-	return
-def stateHandle(val):
-	wpamon.setstate(val)
-	return
-def ipHandle(val):
-	wpamon.setip(val)
-	return
-def qualHandle(val):
-	wpamon.setqual(val)
-	return
-
-def doparse(val):
-	try:
-		key,data = re.split("=",val)
-	except ValueError:
-		return
-	if key in retvals:
-		retvals[key](data)
 
 class wpaclass:
 	def __init__(self):
@@ -38,6 +12,12 @@ class wpaclass:
 		self.ip = ""
 		self.qual = ""
 		self.sock = None
+		self.fnmap={'bssid':self.setbssid,
+			    'ssid':self.setssid,
+			    'key_mgmt':self.setkeymgmt,
+			    'wpa_state':self.setstate,
+			    'ip_address':self.setip,
+			    'qual':self.setqual}
 		return
 	def setbssid(self,bssid):
 		if bssid != self.bssid:
@@ -49,7 +29,13 @@ class wpaclass:
 		data = self.sock.recv(65535)
 		splitdata = re.split("[\r\n]+",data)
 		for line in splitdata:
-			doparse(line)
+			try:
+				key,data = re.split("=",line)
+			except ValueError:
+				continue
+			if key in self.fnmap:
+				self.fnmap[key](data)
+			
 	def setssid(self,ssid):
 		self.ssid = ssid
 	def setkeymgmt(self,mgmt):
@@ -66,14 +52,6 @@ class wpaclass:
 		return str(self)
 		
 		
-
-retvals={'bssid':bssidHandle,
-	 'ssid':ssidHandle,
-	 'key_mgmt':mgmtHandle,
-	 'wpa_state':stateHandle,
-	 'ip_address':ipHandle,
-	 'qual':qualHandle}
-
 wpamon = wpaclass()
 wpamon.sock =socket.socket(socket.AF_UNIX,socket.SOCK_DGRAM,0)
 
@@ -83,5 +61,8 @@ wpamon.sock.bind("/tmp/%d" % os.getpid())
 wpamon.sock.send("STATUS")
 wpamon.runparse()
 print wpamon
-
+while 1:
+	wpamon.runparse()
+	print wpamon
+wpamon.sock.shutdown(socket.SHUT_RDWR)
 os.remove("/tmp/%d" % os.getpid())

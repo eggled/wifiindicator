342d22f5076bb7b1d8d0c3e94077751786728e04
Dan Eggleston <eggled@gmail.com>
2010-12-20 10:10:33 -0600

    Made up a threaded icon updating app

--- ind.py
+++ ind.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python
-import gtk,time,threading
+import gtk,time,threading,os,signal
 
 def watchwpa():
 	print "Doing my sleeping"
-	time.sleep(5)
+	watchwpa.n.set_from_file("icon/wifi0.png")
+	time.sleep(2)
+	watchwpa.n.set_from_file("icon/wifi1.png")
+	time.sleep(2)
+	watchwpa.n.set_from_file("icon/wifi2.png")
+	time.sleep(2)
+	watchwpa.n.set_from_file("icon/wifi3.png")
+	time.sleep(2)
+	watchwpa.n.set_from_file("icon/wifi4.png")
+	time.sleep(2)
 	print "All done!"
+	gtk.main_quit()
 	return
 
 k = threading.Thread(target=watchwpa)
 k.start()
 
+class myStatusIcon(gtk.StatusIcon):
+	def __init__(self):
+		gtk.StatusIcon.__init__(self)
+def activate(self):
+	print "Activated!"
+def doquit(self):
+	print "Quitting!"
+	os.kill(os.getpid(),signal.SIGTERM)
+	
+def popup(self,button,activate_time):
+	print "Creating the menu"
+	pop = gtk.Menu()
+	quit = gtk.MenuItem("Quit")
+	quit.connect("activate",doquit)
+	pop.append(quit)
+	pop.show_all()
+	pop.popup(None,None,None,button,activate_time,None)
+
 gtk.gdk.threads_init()
-n = gtk.StatusIcon()
+n = myStatusIcon()
+n.connect("activate",activate)
+n.connect("popup-menu",popup)
+watchwpa.n = n
 n.set_from_file("/usr/share/pixmaps/gwibber.svg")
 n.set_tooltip_text("Egg-Wireless")
 

7a0540610ab3c2ee860941feb2cb963218581575
Dan Eggleston <eggled@gmail.com>
2010-12-20 14:50:27 -0600

    Added inotify support to the wpa socket listener

--- wpasock.py
+++ wpasock.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import socket,os,sys,re
+import socket,os,sys,re,threading,inotify,time
 
 
 class wpaclass:
@@ -28,21 +28,29 @@ class wpaclass:
 	def runparse(self):
 		try:
 			data = self.sock.recv(65535)
-		except socket.timeout:
-			self.sock.send("PING")
+		except:
+			try:
+				self.sock.send("PING")
+			except:
+				pass
 			try:
 				data = self.sock.recv(65535)
-			except socket.timeout:
+			except:
 				print "Disconnected!"
-				sys.exit(0)
+				return
 		splitdata = re.split("[\r\n]+",data)
 		for line in splitdata:
 			try:
 				key,data = re.split("=",line)
 			except ValueError:
+				if re.search("CTRL-EVENT-CONNECTED",line):
+					self.sock.send("STATUS")
+					self.runparse()
 				continue
 			if key in self.fnmap:
 				self.fnmap[key](data)
+			else:
+				print "No key for %s" % line
 			
 	def setssid(self,ssid):
 		self.ssid = ssid
@@ -50,6 +58,12 @@ class wpaclass:
 		self.keymgmt = mgmt
 	def setstate(self,state):	
 		self.state = state
+		if state != "COMPLETED":
+			self.bssid=""
+			self.ssid=""
+			self.keymgmt = ""
+			self.ip = ""
+			self.qual = ""
 	def setip(self,ip):
 		self.ip = ip
 	def setqual(self,qual):
@@ -59,20 +73,38 @@ class wpaclass:
 	def __repr__(self):
 		return str(self)
 	def doconnect(self,path):
-		self.sock =socket.socket(socket.AF_UNIX,socket.SOCK_DGRAM,0)
-
-		self.sock.connect(path)
-		self.sock.bind("/tmp/%d" % os.getpid())
-
-		self.sock.send("ATTACH")
-		self.sock.send("STATUS")
-		self.sock.settimeout(1)
-		self.runparse()
+		try:
+			if not self.sock:
+				self.sock =socket.socket(socket.AF_UNIX,socket.SOCK_DGRAM,0)
+				self.sock.bind("/tmp/%d" % os.getpid())
+			self.sock.connect(path)
+			self.sock.send("ATTACH")
+			self.sock.send("STATUS")
+			self.sock.settimeout(1)
+			self.runparse()
+		except:
+			time.sleep(0.5)
+			self.doconnect(path)
+	def lackingData(self):
+		if self.state != "COMPLETED":
+			return None
+		if not self.ip or not self.qual or not self.bssid:
+			return 1
+		if not self.ssid or not self.keymgmt:
+			return 1
 wpamon = wpaclass()
-wpamon.doconnect("/var/run/wpa_supplicant/wlan0")
+def establish():
+	wpamon.doconnect("/var/run/wpa_supplicant/wlan0")
+establish()
+th = threading.Thread(target=inotify.initwatch,kwargs={'callback':establish})
+th.start()
 print wpamon
 while 1:
 	wpamon.runparse()
 	print wpamon
+	if wpamon.lackingData():
+		print "Lacking, doing a status update!"
+		time.sleep(1) ## Limit how often we'll update
+		wpamon.sock.send("STATUS")
 wpamon.sock.shutdown(socket.SHUT_RDWR)
 os.remove("/tmp/%d" % os.getpid())
